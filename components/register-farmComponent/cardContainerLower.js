053befd0d8b6980c8ac24d3a917e6738d4a5d132
engineerpawangupta <pawan@radaiansys.com>
2024-08-17 20:03:54 +0530

    corrected the imported

--- components/register-farmComponent/cardContainerLower.js
+++ components/register-farmComponent/cardContainerLower.js
@@ -1,15 +1,15 @@
 import React from "react";
 import CardComponentLower from "./cardComponentLower";
-import Image1 from "../../public/farmFormIcons/LowerComponent/1.png";
-import Image2 from "../../public/farmFormIcons/LowerComponent/2.png";
-import Image3 from "../../public/farmFormIcons/LowerComponent/3.png";
-import Image4 from "../../public/farmFormIcons/LowerComponent/4.png";
-import Image5 from "../../public/farmFormIcons/LowerComponent/5.png";
-import Image6 from "../../public/farmFormIcons/LowerComponent/6.png";
-import Image7 from "../../public/farmFormIcons/LowerComponent/7.png";
-import Image8 from "../../public/farmFormIcons/LowerComponent/8.png";
-import Image9 from "../../public/farmFormIcons/LowerComponent/9.png";
-import Image10 from "../../public/farmFormIcons/LowerComponent/10.png";
+import Image1 from "/farmFormIcons/LowerComponent/1.png";
+import Image2 from "/farmFormIcons/LowerComponent/2.png";
+import Image3 from "/farmFormIcons/LowerComponent/3.png";
+import Image4 from "/farmFormIcons/LowerComponent/4.png";
+import Image5 from "/farmFormIcons/LowerComponent/5.png";
+import Image6 from "/farmFormIcons/LowerComponent/6.png";
+import Image7 from "/farmFormIcons/LowerComponent/7.png";
+import Image8 from "/farmFormIcons/LowerComponent/8.png";
+import Image9 from "/farmFormIcons/LowerComponent/9.png";
+import Image10 from "/farmFormIcons/LowerComponent/10.png";
 
 const images = [
   Image1,

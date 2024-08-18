090eb4b5aea22a950dcd01e26aaa2fc10c3bf142
engineerpawangupta <pawan@radaiansys.com>
2024-08-18 12:04:05 +0530

    feat: image added

--- components/register-farmComponent/cardComponentLower.js
+++ components/register-farmComponent/cardComponentLower.js
@@ -1,31 +1,4 @@
-import Image from "next/image";
 import React from "react";
-
-// import React from "react";
-// import CardComponentLower from "./cardComponentLower";
-// import Image1 from "../../public/farmFormIcons/LowerComponent/1.png";
-// import Image2 from "../../public/farmFormIcons/LowerComponent/2.png";
-// import Image3 from "../../public/farmFormIcons/LowerComponent/3.png";
-// import Image4 from "../../public/farmFormIcons/LowerComponent/4.png";
-// import Image5 from "../../public/farmFormIcons/LowerComponent/5.png";
-// import Image6 from "../../public/farmFormIcons/LowerComponent/6.png";
-// import Image7 from "../../public/farmFormIcons/LowerComponent/7.png";
-// import Image8 from "../../public/farmFormIcons/LowerComponent/8.png";
-// import Image9 from "../../public/farmFormIcons/LowerComponent/9.png";
-// import Image10 from "../../public/farmFormIcons/LowerComponent/10.png";
-
-// const images = [
-//   Image1,
-//   Image2,
-//   Image3,
-//   Image4,
-//   Image5,
-//   Image6,
-//   Image7,
-//   Image8,
-//   Image9,
-//   Image10,
-// ];
 
 const CardComponentLower = ({ key, image, topic, description }) => {
   return (
--- components/register-farmComponent/cardContainerLower.js
+++ components/register-farmComponent/cardContainerLower.js
@@ -1,4 +1,5 @@
-
+import React from "react";
+import CardComponentLower from "./cardComponentLower";
 
 const CardContainerLower = ({ cardData }) => {
   return (

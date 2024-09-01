77687e58762a587287f903e0b48218eb4b36c04d
engineerpawangupta <pawan@radaiansys.com>
2024-09-01 17:47:19 +0530

    image import corrected

--- components/register-farmComponent/cardContainerLower.js
+++ components/register-farmComponent/cardContainerLower.js
@@ -1,31 +1,19 @@
 import React from "react";
 import CardComponentLower from "./cardComponentLower";
-import Image1 from "../../public/farmFormIcons/LowerComponent/1.png";
-import Image2 from "../../public/farmFormIcons/LowerComponent/2.png";
-import Image3 from "../../public/farmFormIcons/LowerComponent/3.png";
-import Image4 from "../../public/farmFormIcons/LowerComponent/4.png";
-import Image5 from "../../public/farmFormIcons/LowerComponent/5.png";
-import Image6 from "../../public/farmFormIcons/LowerComponent/6.png";
-import Image7 from "../../public/farmFormIcons/LowerComponent/7.png";
-import Image8 from "../../public/farmFormIcons/LowerComponent/8.png";
-import Image9 from "../../public/farmFormIcons/LowerComponent/9.png";
-import Image10 from "../../public/farmFormIcons/LowerComponent/10.png";
-
 const images = [
-  Image1,
-  Image2,
-  Image3,
-  Image4,
-  Image5,
-  Image6,
-  Image7,
-  Image8,
-  Image9,
-  Image10,
+  "/farmFormIcons/LowerComponent/1.png",
+  "/farmFormIcons/LowerComponent/2.png",
+  "/farmFormIcons/LowerComponent/3.png",
+  "/farmFormIcons/LowerComponent/4.png",
+  "/farmFormIcons/LowerComponent/5.png",
+  "/farmFormIcons/LowerComponent/6.png",
+  "/farmFormIcons/LowerComponent/7.png",
+  "/farmFormIcons/LowerComponent/8.png",
+  "/farmFormIcons/LowerComponent/9.png",
+  "/farmFormIcons/LowerComponent/10.png",
 ];
 
 const CardContainerLower = ({ cardData }) => {
-
   return (
     <div className="flex flex-wrap justify-center items-center  gap-y-10 md:gap-x-10 mt-10">
       {cardData.map((item, index) => (
@@ -36,7 +24,6 @@ const CardContainerLower = ({ cardData }) => {
           description={item.description}
         />
       ))}
-
     </div>
   );
 };

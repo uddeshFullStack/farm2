f5124a96062203692793bdc2b73e09b7192249c7
engineerpawangupta <pawan@radaiansys.com>
2024-09-10 23:38:49 +0530

    changes the image configue

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
-  const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      domains: ['iexypggcyfvjcyynccbi.supabase.co'], // Add your Supabase domain here
-    },
-  }
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "iexypggcyfvjcyynccbi.supabase.co",
+      "https://saran-farm-git-main-engineerpawanguptas-projects.vercel.app/",
+    ],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    path: "/_next/image",
+    loader: "default",
+  },
+};
 
-  module.exports = nextConfig
+module.exports = nextConfig;

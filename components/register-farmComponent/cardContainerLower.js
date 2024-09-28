1469499d1f0dc02dd561c477b5b8fb892932c6af
engineerpawangupta <pawan@radaiansys.com>
2024-09-28 22:22:24 +0530

    feat: removed image import

--- components/register-farmComponent/cardContainerLower.js
+++ components/register-farmComponent/cardContainerLower.js
@@ -1,17 +1,6 @@
 import React from "react";
 import CardComponentLower from "./cardComponentLower";
-// const images = [
-//   "/farmFormIcons/LowerComponent/1.png",
-//   "/farmFormIcons/LowerComponent/2.png",
-//   "/farmFormIcons/LowerComponent/3.png",
-//   "/farmFormIcons/LowerComponent/4.png",
-//   "/farmFormIcons/LowerComponent/5.png",
-//   "/farmFormIcons/LowerComponent/6.png",
-//   "/farmFormIcons/LowerComponent/7.png",
-//   "/farmFormIcons/LowerComponent/8.png",
-//   "/farmFormIcons/LowerComponent/9.png",
-//   "/farmFormIcons/LowerComponent/10.png",
-// ];
+
 
 const CardContainerLower = ({ cardData }) => {
   return (

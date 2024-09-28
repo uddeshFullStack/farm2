d0bc02c9e6c6a0d7c851d92a8a46f5f722704a49
engineerpawangupta <pawan@radaiansys.com>
2024-09-28 22:11:43 +0530

    image import corrected

--- components/register-farmComponent/cardComponentLower.js
+++ components/register-farmComponent/cardComponentLower.js
@@ -1,31 +1,25 @@
+import Image from "next/image";
 import React from "react";
-import CardComponentLower from "./cardComponentLower";
-const images = [
-  "/farmFormIcons/LowerComponent/1.png",
-  "/farmFormIcons/LowerComponent/2.png",
-  "/farmFormIcons/LowerComponent/3.png",
-  "/farmFormIcons/LowerComponent/4.png",
-  "/farmFormIcons/LowerComponent/5.png",
-  "/farmFormIcons/LowerComponent/6.png",
-  "/farmFormIcons/LowerComponent/7.png",
-  "/farmFormIcons/LowerComponent/8.png",
-  "/farmFormIcons/LowerComponent/9.png",
-  "/farmFormIcons/LowerComponent/10.png",
-];
-
-const CardContainerLower = ({ cardData }) => {
+const CardComponentLower = ({ key, topic, description, image }) => {
   return (
-    <div className="flex flex-wrap justify-center items-center  gap-y-10 md:gap-x-10 mt-10">
-      {cardData.map((item, index) => (
-        <CardComponentLower
-          key={index}
-          image={images[index]}
-          topic={item.topic}
-          description={item.description}
-        />
-      ))}
+    <div className="h-50 " style={{ width: "420px" }}>
+      <div className="p-4 rounded-md xl:flex md:flex">
+        <div className="flex justify-center items-start">
+          <Image
+            width={64}
+            height={64}
+            src={image}
+            alt="icon"
+            className="bg-backgroundColor-image p-4 rounded-md"
+          />
+        </div>
+        <div className="ml-4">
+          <div className="font-bold text-lg text-primary-colour">{topic}</div>
+          <div className=" text-justify pt-1">{description}</div>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default CardContainerLower;
+export default CardComponentLower;
--- components/register-farmComponent/cardContainerLower.js
+++ components/register-farmComponent/cardContainerLower.js
@@ -1,12 +1,25 @@
 import React from "react";
 import CardComponentLower from "./cardComponentLower";
+const images = [
+  "/farmFormIcons/LowerComponent/1.png",
+  "/farmFormIcons/LowerComponent/2.png",
+  "/farmFormIcons/LowerComponent/3.png",
+  "/farmFormIcons/LowerComponent/4.png",
+  "/farmFormIcons/LowerComponent/5.png",
+  "/farmFormIcons/LowerComponent/6.png",
+  "/farmFormIcons/LowerComponent/7.png",
+  "/farmFormIcons/LowerComponent/8.png",
+  "/farmFormIcons/LowerComponent/9.png",
+  "/farmFormIcons/LowerComponent/10.png",
+];
+
 const CardContainerLower = ({ cardData }) => {
   return (
     <div className="flex flex-wrap justify-center items-center  gap-y-10 md:gap-x-10 mt-10">
       {cardData.map((item, index) => (
         <CardComponentLower
           key={index}
-          image={item.image}
+          image={images[index]}
           topic={item.topic}
           description={item.description}
         />
